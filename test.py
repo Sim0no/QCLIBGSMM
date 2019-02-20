ccbeb1f9272693e1368cd64af99a833e298b5af8
Simono2000 <46967217+Simono2000@users.noreply.github.com>
2019-02-20 09:55:10 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest,complejo,math as m
+class TestStringMethods(unittest.TestCase):
+    def test_deveriaFallar(self):
+        m2,m3=[[(7,0),(6,5)],[(6,-5),(0,-3)]],[[(7,0),(6,5)],[(6,-5),(-3,0)]]
+        self.assertFalse(complejo.esHermitiana(m2))
+        self.assertFalse(complejo.esUnitaria(m3))
+    def test_deveriaServir(self):
+        m3,m4=[[(7,0),(6,5)],[(6,-5),(-3,0)]],[[(m.cos(30),0),(-1*m.sin(30),0),(0,0)],[(m.sin(30),0),(m.cos(30),0),(0,0)],[(0,0),(0,0),(1,0)]]
+        m5,m6 = [[(3,2),(5,-1),(0,2)],[(0,0),(12,0),(6,-3)],[(2,0),(4,4),(9,3)]],[[(1,0),(3,4),(5,-7)],[(10,2),(6,0),(2,5)],[(0,0),(1,0),(2,9)]]
+        self.assertEquals(m5,complejo.matrizAdjunta(complejo.matrizAdjunta(m5)))
+        self.assertTrue(complejo.esHermitiana(m3))
+        self.assertTrue(complejo.esUnitaria(m4))
+        self.assertTrue(len(m5)*len(m6) == len(complejo.productoTensor(m5,m6)))
+if __name__ == '__main__':
+    unittest.main()
