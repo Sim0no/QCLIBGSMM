6d26fda77bcc3fa0923ed8aeac2ed8ea63b17fc2
Simono2000 <46967217+Simono2000@users.noreply.github.com>
2019-03-06 09:19:29 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import unittest,complejo,math as m
 class TestStringMethods(unittest.TestCase):
-    def test_deveriaFallar(self):
+    def test_deberiaFallar(self):
         m2,m3=[[(7,0),(6,5)],[(6,-5),(0,-3)]],[[(7,0),(6,5)],[(6,-5),(-3,0)]]
         self.assertFalse(complejo.esHermitiana(m2))
         self.assertFalse(complejo.esUnitaria(m3))
-    def test_deveriaServir(self):
+    def test_deberiaServir(self):
         m3,m4=[[(7,0),(6,5)],[(6,-5),(-3,0)]],[[(m.cos(30),0),(-1*m.sin(30),0),(0,0)],[(m.sin(30),0),(m.cos(30),0),(0,0)],[(0,0),(0,0),(1,0)]]
         m5,m6 = [[(3,2),(5,-1),(0,2)],[(0,0),(12,0),(6,-3)],[(2,0),(4,4),(9,3)]],[[(1,0),(3,4),(5,-7)],[(10,2),(6,0),(2,5)],[(0,0),(1,0),(2,9)]]
         self.assertEquals(m5,complejo.matrizAdjunta(complejo.matrizAdjunta(m5)))
