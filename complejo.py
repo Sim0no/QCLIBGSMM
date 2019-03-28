0ee002cfc5392097eba72ec1823ba98cc16adb97
Simono2000 <46967217+Simono2000@users.noreply.github.com>
2019-03-28 01:52:48 -0500

    Update complejo.py

--- complejo.py
+++ complejo.py
@@ -14,6 +14,7 @@ def suma(tupla1,tupla2):
     return (float(a1+a2),float(b1+b2))
 
 
+
 def resta(tupla1,tupla2):
     '''Retorna la resta de 2 numeros complejos, el primer elemento de cada
         tupla representa la parte real del numero,el otro elemento representa
@@ -21,6 +22,7 @@ def resta(tupla1,tupla2):
         (a1,b1i)-(a2,b2i) ----->(a1-a2,(b1-b2)i) '''
     a1,b1,a2,b2 = tupla1[0],tupla1[1],tupla2[0],tupla2[1]
     return (float(a1-a2),float(b1-b2))
+
 
 
 def multiplicacion(tupla1,tupla2):
@@ -31,6 +33,7 @@ def multiplicacion(tupla1,tupla2):
     a1,b1,a2,b2 = tupla1[0],tupla1[1],tupla2[0],tupla2[1]
     return (float((a1*a2)-(b1*b2)),float((a1*b2)+(a2*b1)))
 def division(tupla1,tupla2):
+    
     try:
         '''Retorna la division de 2 numeros complejos, el primer elemento de cada
             tupla representa la parte real del numero,el otro elemento representa
@@ -38,9 +41,11 @@ def division(tupla1,tupla2):
             (a1,b1i)/(a2,b2i) ----->((a1*a2+b1*b2)/((a2)**2+(b2)**2),((a2*b1)-(a1*b2))/((a2)**2+(b2**2)))'''
         a1,b1,a2,b2 = tupla1[0],tupla1[1],tupla2[0],tupla2[1]
         return (round((a1*a2+b1*b2)/((a2)**2+(b2)**2),3),round(((a2*b1)-(a1*b2))/((a2)**2+(b2)**2),3))
+    
     except ZeroDivisionError:print('La division entre estos 2 numeros no esta definida.')
         
-        
+    
+    
 def modulo(tupla):
     '''Retorna el modulo de un numero complejo, el primer elemento de la
             tupla representa la parte real del numero,el otro elemento representa
@@ -50,6 +55,7 @@ def modulo(tupla):
     return ((a)**2+(b)**2)**0.5
 
 
+
 def conjugado(tupla):
     '''Retorna el conjugado de un numero complejo, el primer elemento de la
             tupla representa la parte real del numero,el otro elemento representa
@@ -57,6 +63,7 @@ def conjugado(tupla):
             conjugado(a + bi) -----> a - bi'''
     a,b = tupla[0],tupla[1]
     return (float(a),float(b*-1))
+
 
 
 def polar(tupla):
@@ -70,6 +77,7 @@ def polar(tupla):
     return (round(z,3),round(angulo,3))
 
 
+
 def cartesiano(tupla):
     '''Convierte un numero complejo de su forma polar a binomica/cartesiana,el primer elemento
         de la tupla representa la magnitud,el otro elemento representa
@@ -81,11 +89,13 @@ def cartesiano(tupla):
     return (round(a,3),round(b,3))
 
 
+
 def fase(tupla):
     '''Retorna la fase/direccion (en radianes) de un numero complejo.'''
     a,b = tupla[0],tupla[1]
     angulo = arco_tangente(b/a)
     return (round(angulo,3))
+
 
 
 def polarGrados(tupla):
@@ -109,6 +119,7 @@ def polarGrados(tupla):
     return (round(z,3),round(angulo,3))
 
 
+
 def cartesianoGrados(tupla):
     '''Convierte un numero complejo de su forma polar a binomica/cartesiana,el primer elemento
         de la tupla representa la magnitud,el otro elemento representa
@@ -120,7 +131,9 @@ def cartesianoGrados(tupla):
     b = magnitud*sin(angulo)
     return (round(a,3),round(b,3))
 
+
 ##############VECTORES Y MATRICES
+
 
 def sumaVectores(vector1,vector2):
     '''Se ingresa cada vector como [],
@@ -135,6 +148,7 @@ def sumaVectores(vector1,vector2):
     return aux     
 
 
+
 def restaVectores(vector1,vector2):
     '''Se ingresa cada vector como [],
     cada componente de el vector es una tupla que contiene
@@ -145,6 +159,7 @@ def restaVectores(vector1,vector2):
     for i in range(len(vector1)):
         aux.append(resta(vector1[i],vector2[i]))
     return aux
+
 
 
 def multiplicacionVectorEscalar(vector,escalar):
@@ -158,6 +173,7 @@ def multiplicacionVectorEscalar(vector,escalar):
     return aux
 
 
+
 def sumaMatricesComplejas(matriz1,matriz2):
     '''Entran 2 matrices de M x N, retorna la suma de cada matriz
     compleja.'''
@@ -168,6 +184,8 @@ def sumaMatricesComplejas(matriz1,matriz2):
     return aux
     
 
+    
+    
 def restaMatricesComplejas(matriz1,matriz2):
     '''Entran 2 matrices de M x N, retorna la resta de cada matriz
     compleja.'''
@@ -178,6 +196,7 @@ def restaMatricesComplejas(matriz1,matriz2):
     return aux
 
 
+
 def multiplicacionMatrizEscalar(matriz,escalar):
     '''Se ingresa la matriz ,y el escalar conmplejo
     como tupla,cada componente de la matriz es un vector complejo, retorna la multiplicación
@@ -186,6 +205,7 @@ def multiplicacionMatrizEscalar(matriz,escalar):
     for i in range(len(matriz)):
         aux.append(multiplicacionVectorEscalar(matriz[i],escalar))
     return aux
+
 
 
 def matrizTranspuesta(matriz):
@@ -199,6 +219,7 @@ def matrizTranspuesta(matriz):
     return aux
 
 
+
 def matrizConjugada(matriz):
     '''Entra una matriz , retorna su mtariz conjugada asociada '''
     aux = []
@@ -210,9 +231,11 @@ def matrizConjugada(matriz):
     return aux
 
 
+
 def matrizAdjunta(matriz):
     '''Entra una matriz, retorna la matriz adjunta asociada'''
     return matrizTranspuesta(matrizConjugada(matriz))
+
 
 
 def multiplicacionMatrices(A,B):
@@ -232,7 +255,13 @@ def multiplicacionMatrices(A,B):
                     matriz[i][j] = (m[0]+n[0], m[1]+n[1])
         return matriz
     else:
+        
         raise 'La multiplicación de matrices no está definida para estas matrices'
+        
+        
+        
+        
+        
 def multiplicacionMatricesNormales(matriz1,matriz2):
     '''Entran 2 matrices, una de M x I, la otra de I X N, retorna
         la multiplicacion de lamatriz1 por la matriz 2 de dimension M X N'''
@@ -248,6 +277,9 @@ def multiplicacionMatricesNormales(matriz1,matriz2):
             aux[i][j] = summ
     return aux
 
+
+
+
 def productoInternoVectores(vector1,vector2):
     '''Se ingresan 2 vectores complejos de longitud n, retorna el producto interno entre estos'''
     if len(vector1) != len(vector2): raise 'Los vectores no tienen la misma longitud, su producto interno no esta definido'
@@ -257,6 +289,8 @@ def productoInternoVectores(vector1,vector2):
     return aux;
 
 
+
+
 def moduloVector(vector):
     '''Se ingresa un vector complejo, retorna el modulo del vector'''
     aux = 0
@@ -264,16 +298,23 @@ def moduloVector(vector):
         aux += modulo(i)**2
     return round(aux**0.5,3)
 
+
+
+
 def distanciaEntreVectores(vector1,vector2):
     '''Se ingresan 2 vectores complejos de longitud n, retorna la distancia entre estos'''
     if len(vector1) != len(vector2): raise 'Los vectores no tienen la misma longitud, su producto interno no esta definido'
     return moduloVector(restaVectores(vector1,vector2))
+
+
 
         
 def esHermitiana(matriz):
     '''Entra una matriz cuadrada, Retorna verdadero  si la matriz es hermitiana(igual a su propia traspuesta conjugada)'''
     if len(matriz) != len(matriz[0]):  raise 'La matriz no es cuadrada'
     return matriz == matrizAdjunta(matriz)
+
+
 
 
 def esUnitaria(matriz):
@@ -283,6 +324,8 @@ def esUnitaria(matriz):
     for k in range(len(i)):
         i[k][k] = (float(1),float(0))
     return multiplicacionMatrices(matriz,matrizAdjunta(matriz)) == multiplicacionMatrices(matrizAdjunta(matriz),matriz) == i
+
+
 
 
 def productoTensor(matriz1,matriz2):
@@ -307,6 +350,8 @@ def productoTensor(matriz1,matriz2):
 
 #######de lo clásico a lo cuántico
 
+
+
 def marbels(matrizAdj, estadoInicial, clicks):
     '''Se simula el experimento de  las canicas despues de varios clicks'''
     while clicks > 0:
@@ -319,6 +364,7 @@ def marbels(matrizAdj, estadoInicial, clicks):
             aux.append(sume)
         estadoInicial  = aux
     return aux
+
 
 
 def barras(matrizAdj, estadoInicial, clicks):
@@ -335,6 +381,7 @@ def barras(matrizAdj, estadoInicial, clicks):
     return aux
 
 
+
 def dobleRendija(matriz,clicks):
     '''Se realiza el esperimento de la doble rendija'''
     for i in range(len(matriz)):
@@ -343,6 +390,10 @@ def dobleRendija(matriz,clicks):
                 matriz[i][j] = multiplicacion(matriz[i][j],matriz[i][j])
             matriz[i][j] = modulo(matriz[i][j])
     return matriz
+
+
+
+
 def dobleRendijaBalas(matriz,posicion,clicks):
     '''Se realiza el esperimento de la doble rendija, matriz representa el estado inicial del sistema,
         posicion representa la posicion inicial de la bala, clicks el numero de clicks'''
@@ -353,12 +404,16 @@ def dobleRendijaBalas(matriz,posicion,clicks):
 
 
 
+
 def superPosition(vector,n):
     '''Entra como parametro un vector y un numero n, retornamos la probabilidad de que nuestra particula
     se encuentre en la posicion n.'''
     moduloo = moduloVector(vector)
     particula = vector[n]
     return round((modulo(particula)/moduloo)**2,6)
+
+
+
 
 def amplitudeOfTransition(v1,v2):
     '''La función recibe como parametro 2 escalares y 2 vectores, la función nos retorna
@@ -367,12 +422,19 @@ def amplitudeOfTransition(v1,v2):
     for i in range(len(v1)):
         v1[i] = conjugado(v1[i])
     return productoInternoVectores(v1,v2)
+
+
+
 def identidad(n):
     '''Nos retorna la matriz idenidad en C*n'''
     x = [[(0,0) for i in range(n)] for j in range(n)]
     for i in range(n):
         x[i][i] = (1,0)
     return x
+
+
+
+
 def valorPromedio(matriz,ket):
     '''Retornamos el valor promedio asociado al sistema'''
     kat = []
@@ -386,6 +448,10 @@ def valorPromedio(matriz,ket):
             aux.append(j)
 
     return productoInternoVectores(ket,aux)
+
+
+
+
 def varianza(matriz,ket):
     '''Esta funcion calcula la varianza dada una matriz de estado y su correspondiente vector ket
     retorna un numero complejo'''
@@ -401,7 +467,9 @@ def varianza(matriz,ket):
         return ans         
     else:raise 'La matriz no es Hermitiana'
 
-    
+
+        
+        
 def valoresPropios(observable):
     '''retorna un arreglo con los valores propios del observable'''
     aux = []
@@ -418,6 +486,8 @@ def valoresPropios(observable):
     for i in valores:
         rta.append(i)
     return rta
+
+
 
 
 def vectoresPropios(observable):
@@ -455,6 +525,8 @@ def vectoresPropios(observable):
     return rta
 
 
+
+
 def probabilidadTransitar(observable,estado):
     '''Entran como parametro una matriz observable y un vector de estado,
     retorna la probabilidad de que el sistema transite a alguno de los vectores
@@ -478,6 +550,8 @@ def probabilidadTransitar(observable,estado):
     return ans
 
 
+
+
 def dinamicaSistema(n,M,estado):
     '''n es el numero de pasos n, se va calcular el estado final despues de
     que se haya aplicado la secuencia completa de la matriz M n veces'''
@@ -485,6 +559,6 @@ def dinamicaSistema(n,M,estado):
     for i in range(n):
         estado = multiplicacionMatrices(estado,M)
     return estado[0]
-        
+       
     
     
